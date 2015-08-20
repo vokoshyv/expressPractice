4f8d8b1efc33b85abd726d0146c4ecbd2807a19a
vokoshyv <ronsuij@gmail.com>
2015-08-20 12:27:39 -0700

    initializes file for index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+// some tools to manipulate files
+var fs = require('fs');
+var _ = require('lodash');
+
+var users = [];
+
+fs.readFile('toUseInExpressApp/users.json', {encoding: 'utf8'}, function(err, data){
+  if (err) throw err;
+
+  JSON.parse(data).forEach(function(user){
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
+    users.push(user);
+  })
+})
+
+
+
+
+app.get('/', function(req, res, next){
+  res.send(JSON.stringify(users, null, 2));
+});
+
+app.get('/yo', function(req, res, next){
+  res.send('I say yo!');
+});
+
+var server = app.listen(3000, function(){
+  console.log('Server is running at localhost:' + server.address().port);
+});
